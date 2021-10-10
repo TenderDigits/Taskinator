6e04e542ac8e40d42d1f6d1e2aeb3f1a7708c102
Sergio Lamar <sergio.lamar@gmail.com>
2021-10-10 14:39:13 -0700

    form submission feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
 // "Add Task" Button
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // Task handler for adding task to ul
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
@@ -11,6 +13,6 @@ var createTaskHandler = function() {
 }
 
 // "Add Task" button tied to add task function
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("click", createTaskHandler);
 
 
